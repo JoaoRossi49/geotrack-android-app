97f8a2eafbcef78200e1484ce499b0fd960b50b1
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-05-07 23:30:22 -0300

    feat: requisição a api de login ao verbo POST, autenticação

--- app/src/main/java/com/android/geotrack/MainActivity.java
+++ app/src/main/java/com/android/geotrack/MainActivity.java
@@ -12,12 +12,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+import android.util.Log;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
     private EditText passwordInput;
     private Button signInBtn;
     private TextView result;
+    private String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +47,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void makeApiCall(String url){
+        try {
+            JSONObject postData = new JSONObject();
+            postData.put("username", userInput.getText());
+            postData.put("password", passwordInput.getText());
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+
+                    try {
+                        Log.i("API", "Resposta recebida com sucesso");
+                        Log.i("API", response.getString("authenticated"));
+                        String auth = response.getString("authenticated");
+                        if(auth == "true"){
+                            result.setText("Usuário autenticado com sucesso!");
+                        }else{
+                            result.setText("Usuário não autenticado!");
+                        }
+
+                    } catch (Exception e) {
+
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("API", "Ocorreu um erro na chamada da API");
+                    Log.e("API", error.getMessage());
+                }
+            });
+            Volley.newRequestQueue(this).add(request);
+        }catch(Exception e){
+
+        }
+    }
+
     public void logIn(View view){
-        result.setText("Chamou Login!");
+        Log.i("BTN", "Apertou botão");
+        url = "http://192.168.86.6:8000/api/login/";
+        makeApiCall(url);
     }
 
 }
